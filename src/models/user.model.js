84a98a501a1390cf9bba98b7b8a72f7742dca7a1
Space Map Server <spacemap@server>
2022-04-28 09:32:52 +0000

    feat/oauth : Passport적용

---
+++ src/models/user.model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const userScheme = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  provider: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  nickname: {
+    type: String,
+    required: true,
+  },
+  snsId: {
+    type: String,
+    require: true,
+  },
+});
+module.exports = mongoose.model('user', userScheme);
---
+++ src/routes/oauth.route.js
@@ -0,0 +1,7 @@
+const router=require('express').Router()
+
+const wrapper=require('../lib/request-handler')
+
+router.get('/google')
+
+module.exports=router;
