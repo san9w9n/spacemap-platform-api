c57cd081441b2ffe87042453be43f2112b2858ec
hyo0508 <2018008168@hanyang.ac.kr>
2022-07-14 09:35:54 +0900

    remove debugging code

--- src/api/interestedSatellites/interestedSatellites.task.js
+++ src/api/interestedSatellites/interestedSatellites.task.js
@@ -14,8 +14,6 @@ class InterestedSatellitesTask {
     this.period = '0 0 0 * * *';
     this.interestedSatellitesService = interestedSatellitesService;
     this.handler = this.#sendInterestedConjunctions.bind(this);
-    /* Debugging */
-    this.#sendInterestedConjunctions();
   }
 
   async #sendInterestedConjunctions() {
@@ -61,16 +59,6 @@ class InterestedSatellitesTask {
 module.exports = InterestedSatellitesTask;
 
 /* ISSUES
- * GET subscribe                          -> 완 (필요X)
- * interested가 Primary로                 -> 완
- * html: dca, csv: tca?                   -> 완
- * csv : {pName}_{pid}_{YYYY-MM-DD}.csv   -> 완
- * zip : {YYYY-MM-DD}.zip                 -> 완
- * metadata 형식: Total, 세부, 영어로!    -> 완
- * 보내고 바로 지우기                     -> 완
- * table 분할?: html X, csv O             -> 완
- * table more (toggle)                    -> 완 (필요X)
- * 0개면 안보내기                         -> 완
  * platform primary 정렬
  * forEach, map ??
  * insertMany가 바로 적용 안되는 현상?
--- src/lib/interestedSatellites-handler.js
+++ src/lib/interestedSatellites-handler.js
@@ -49,66 +49,6 @@ class InterestedSatellitesHandler {
     return `${header}${body}${footer}`;
   }
 
-  //   static conjunctionsToHtml(conjunctions, interestedArray) {
-  //     const header = `
-  // <!DOCTYPE html>
-  // <html>
-  //   <head style = "width: 100%"><meta charset="UTF-8" /><title>Dailty Conjunctions</title></head>
-  //   <body style = "width: 100%">
-  //     ${this.#getMetadata(conjunctions, interestedArray)}
-  //     `;
-
-  //     const body = conjunctions
-  //       .map((satellitesId) => {
-  //         const tableHeader = `
-  //       <div style = "width: 100%; display: flex; justify-content: center">
-  //         <div style = "width: 800px; border-radius: 6px; overflow: hidden">
-  //           <table style = "width: 100%; background-color: rgb(235, 235, 235); border-collapse: collapse; border-style: hidden; margin-bottom: 20px">
-  //             <tr style="background-color: rgb(212, 212, 212); font-size: 16px; height: 28px">
-  //               <th>Index</th><th colspan="2">Primary</th><th colspan="2">Secondary</th><th>TCA</th><th>DCA</th>
-  //             </tr>
-  //       `;
-
-  //         const style1 =
-  //           ' style="height: 25px; text-align: center; font-size: 12px"';
-  //         const style2 =
-  //           ' style="height: 25px; text-align: center; font-size: 12px; border-right: 1px solid rgb(219, 219, 219)"';
-  //         const tableBody = satellitesId
-  //           .map(
-  //             (conjunction, index) => `
-  //           <tr${index % 2 ? '' : ' style="background-color: white"'}>
-  //             <td${style2}>${index + 1}</td>
-  //             <td${style1}>${conjunction.pid}</td>
-  //             <td${style2}>${conjunction.pName}</td>
-  //             <td${style1}>${conjunction.sid}</td>
-  //             <td${style2}>${conjunction.sName}</td>
-  //             <td${style2}>${moment
-  //               .utc(conjunction.tcaTime)
-  //               .format('MMM DD, YYYY HH:mm:ss')}</td>
-  //             <td${style1}>${conjunction.dca}</td>
-  //           </tr>
-  //       `,
-  //           )
-  //           .reduce((acc, cur) => acc + cur, '');
-
-  //         const tableFooter = `
-  //         </table>
-  //       </div>
-  //     </div>
-  //       `;
-
-  //         return `${tableHeader}${tableBody}${tableFooter}`;
-  //       })
-  //       .reduce((acc, cur) => acc + cur, '');
-
-  //     const footer = `
-  //   </body>
-  // </html>
-  //     `;
-
-  //     return `${header}${body}${footer}`;
-  //   }
-
   static async conjunctionsToAttachment(conjunctions, email) {
     const date = moment.utc().format('YYYY-MM-DD');
     if (conjunctions.length == 0) {
