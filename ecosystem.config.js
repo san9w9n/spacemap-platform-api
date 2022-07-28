92052c7e6ab62849c47775db2390ff1547eac48a
Seunghwan17 <shchoi.vdrc@gmail.com>
2022-07-28 02:40:51 +0000

    Add: db related with COLA

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,23 +9,6 @@ module.exports = {
         NODE_ENV: 'deployment',
         SPACEMAP_NODE_ENV: 'deployment',
       },
-      env_production: {
-        NODE_ENV: 'production',
-      },
-      watch: false,
-    },
-    {
-      name: 'spacemap-platform-api-services-tasks',
-      script: './src/server.js',
-      instances: 1,
-      exec_mode: 'cluster',
-      env: {
-        NODE_ENV: 'deployment',
-        SPACEMAP_NODE_ENV: 'deployment',
-      },
-      env_production: {
-        NODE_ENV: 'production',
-      },
       watch: false,
     },
     {
@@ -37,8 +20,17 @@ module.exports = {
         NODE_ENV: 'deployment',
         SPACEMAP_NODE_ENV: 'deployment',
       },
-      env_production: {
-        NODE_ENV: 'production',
+      watch: false,
+    },
+    {
+      name: 'spacemap-platform-api-local',
+      script: './src/server.js',
+      instances: 1,
+      exec_mode: 'fork',
+      env: {
+        PORT: 8082,
+        NODE_ENV: 'deployment',
+        SPACEMAP_NODE_ENV: 'local',
       },
       watch: false,
     },
---
+++ src/api/collisionAvoidances/collisionAvoidances.model.js
@@ -0,0 +1,105 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const CollisionAvoidancesScheme = new Schema({
+  createdAt: { type: Date, default: Date.now },
+  email: {
+    type: String,
+    required: true,
+  },
+  predictionEpochTime: {
+    type: Date,
+    required: true,
+  },
+  colaEpochTime: {
+    type: Date,
+    required: true,
+  },
+  pIdOfConjunction: {
+    type: Number,
+    required: true,
+  },
+  sIdOfConjunction: {
+    type: Number,
+    required: true,
+  },
+  startMomentOfCola: {
+    type: Date,
+    required: true,
+  },
+  endMomentOfCola: {
+    type: Date,
+    required: true,
+  },
+  amoutOfLevel: {
+    type: Number,
+    required: true,
+  },
+  numberOfPaths: {
+    type: Number,
+    required: true,
+  },
+  avoidanceLength: {
+    type: Number,
+    required: true,
+  },
+  threshold:{
+    type: Number,
+    required: true,
+  }
+  candidatedPaths: {
+    type: [String],
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ['PENDING', 'ERROR', 'DONE'],
+    required: true,
+    default: 'PENDING',
+  },
+  errorMessage: {
+    type: String,
+    default: undefined,
+  },
+  coladbFilePath: {
+    type: String,
+  },
+});
+
+const ColaTaskScheeme = new Schema({
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  taskId: {
+    type: String,
+    required: true,
+  },
+  s3InputFileKey: {
+    type: [String],
+    required: true,
+  },
+  remoteInputFilePath: {
+    type: [String],
+    required: true,
+  },
+  remoteOutputFilePath: {
+    type: String,
+    required: true,
+  },
+  s3OutputFileKey: {
+    type: String,
+    required: true,
+  },
+});
+
+ColaTaskScheeme.index({ createdAt: -1 });
+
+module.exports = {
+  CollisionAvoidancesModel: mongoose.model(
+    'collisionAvoidances',
+    CollisionAvoidancesScheme,
+  ),
+  ColaTaskModel: mongoose.model('colaTasks', ColaTaskScheeme),
+};
