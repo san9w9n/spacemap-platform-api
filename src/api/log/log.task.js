4bb3d5add8e783af644499dd960ed85149492081
Seunghwan17 <shchoi.vdrc@gmail.com>
2022-08-09 00:21:56 +0000

    Fix: email handler

--- src/api/log/log.task.js
+++ src/api/log/log.task.js
@@ -44,7 +44,15 @@ class LogTask {
     const context = await this.#makeContext();
     const title = `Log Report (${moment.utc().format('MMM DD')})`;
     const html = await SendEmailHandler.renderHtml('logReport', context);
-    await SendEmailHandler.sendMail(['sjb990221@gmail.com'], title, html);
+    await SendEmailHandler.sendMail(
+      [
+        'sjb990221@gmail.com',
+        'shawn.choi@spacemap42.com',
+        'douglas.kim@spacemap42.com',
+      ],
+      title,
+      html,
+    );
   }
 
   async #makeContext() {
