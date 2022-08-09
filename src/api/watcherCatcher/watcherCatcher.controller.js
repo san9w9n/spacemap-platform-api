1e47307930ed524eab5643d7638af3f8cd63f189
Seunghwan17 <shchoi.vdrc@gmail.com>
2022-08-09 07:04:46 +0000

    Fix: error modal(complete)

--- src/api/watcherCatcher/watcherCatcher.controller.js
+++ src/api/watcherCatcher/watcherCatcher.controller.js
@@ -58,7 +58,8 @@ class WatcherCatcherController {
   async predictWatcherCatcher(req, _res) {
     if (!DateHandler.isCalculatableDate()) {
       throw new ForbiddenException(
-        'Not available time.\r\n(Unable time: UTC 15:00 ~ 21:00)',
+        `Not available time.
+        (Unable time: UTC 15:00 ~ 21:00)`,
       );
     }
     const { email } = req.user;
